9d138ec2e0182d21daa99389e22c835e8d1c7fe6
Djura22 <michaeliangroves@gmail.com>
2021-01-07 13:41:29 +0000

    Build: Right paddle added

--- pong.py
+++ pong.py
@@ -17,8 +17,14 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
-
 # Paddle B
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(+350, 0)
 
 
 # Main 
