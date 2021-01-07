2b1765db2f25cfd5500819d6e9eaffe1002021b8
Djura22 <michaeliangroves@gmail.com>
2021-01-07 13:38:56 +0000

    First Commit

---
+++ pong.py
@@ -0,0 +1,26 @@
+
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Djura22")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+
+# Paddle B
+
+
+# Main 
+while True:
+    wn.update()
