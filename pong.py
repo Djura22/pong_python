406f0f23e0f99bacda08c3b8f9c9f716d0524767
Djura22 <michaeliangroves@gmail.com>
2021-01-07 13:43:45 +0000

    Build: Ball added, skeleton finished

--- pong.py
+++ pong.py
@@ -26,6 +26,14 @@ paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(+350, 0)
 
+# Ball
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(0, 0)
+
 
 # Main 
 while True:
