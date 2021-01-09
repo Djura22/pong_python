255f45ff0925d762d274891ad368bdcea1f069bd
Djura22 <michaeliangroves@gmail.com>
2021-01-09 09:53:12 +0000

    Physics: Paddle and Ball collisions written

--- pong.py
+++ pong.py
@@ -1,5 +1,3 @@
-
-
 import turtle
 
 wn = turtle.Screen()
@@ -87,4 +85,14 @@ while True:
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
+        ball.dx *= -1
+
+    # Paddle and Ball Collision Physics
+
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() -50):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if (ball.xcor() < -340 and ball.xcor() < -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() -50):
+        ball.setx(-340)
         ball.dx *= -1
